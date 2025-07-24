24d992d1fd05713735cd02133ec6715e4ef03f96
Manideep <deepanddipyo@gmail.com>
2025-07-24 12:22:31 +0530

    initial commit with main packages

---
+++ src/main/java/com/manideep/spendguide/SpendguideApplication.java
@@ -0,0 +1,13 @@
+package com.manideep.spendguide;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpendguideApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpendguideApplication.class, args);
+	}
+
+}
