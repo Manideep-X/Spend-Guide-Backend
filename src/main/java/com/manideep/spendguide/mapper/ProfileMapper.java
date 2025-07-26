39d3620c0f6512152f7a3c953ee32ed9b5ca9b1a
Manideep <deepanddipyo@gmail.com>
2025-07-26 10:34:35 +0530

    added service layer for user profile for user to register

--- src/main/java/com/manideep/spendguide/mapper/ProfileMapper.java
+++ src/main/java/com/manideep/spendguide/mapper/ProfileMapper.java
@@ -5,6 +5,8 @@ import org.springframework.stereotype.Component;
 import com.manideep.spendguide.dto.ProfileDTO;
 import com.manideep.spendguide.entity.ProfileEntity;
 
+// Mapper class used for converting DTO to Entity object or vise-versa
+// Always returns a new object
 @Component
 public class ProfileMapper {
     
--- src/main/java/com/manideep/spendguide/repository/ProfileRepository.java
+++ src/main/java/com/manideep/spendguide/repository/ProfileRepository.java
@@ -8,6 +8,8 @@ import com.manideep.spendguide.entity.ProfileEntity;
 
 public interface ProfileRepository extends JpaRepository<ProfileEntity, Long> {
 
+    // Optional is used for explicitly handling null return case (recommended)
+    // Internally used query: select * from table_profile where email = ?;
     Optional<ProfileEntity> findByEmail(String email);
 
 }
