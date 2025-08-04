7f5bdac46913dfd2324e9eb2a6203a0a0dbd48cd
Manideep <deepanddipyo@gmail.com>
2025-08-04 17:34:53 +0530

    CORS config is added, user detail service is added for login/authentication

--- src/main/java/com/manideep/spendguide/config/SecurityConfig.java
+++ src/main/java/com/manideep/spendguide/config/SecurityConfig.java
@@ -1,5 +1,7 @@
 package com.manideep.spendguide.config;
 
+import java.util.List;
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.Customizer;
@@ -8,7 +10,9 @@ import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
 @Configuration
 public class SecurityConfig {
@@ -17,14 +21,13 @@ public class SecurityConfig {
     SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
 
         http
-            .cors(Customizer.withDefaults())
-            .csrf(csrf -> csrf.disable()) // Same as .csrf(AbstractHttpConfigurer::disable)
-            .authorizeHttpRequests(configurer -> configurer
-                .requestMatchers("/health", "/info", "/status", "/activate", "/login").permitAll()
-                .anyRequest().authenticated()
-            )
-            // As it is going to be a stateless API because of the use of JWT authentication
-            .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS));
+                .cors(Customizer.withDefaults())
+                .csrf(csrf -> csrf.disable()) // Same as .csrf(AbstractHttpConfigurer::disable)
+                .authorizeHttpRequests(configurer -> configurer
+                        .requestMatchers("/health", "/info", "/status", "/activate", "/login").permitAll()
+                        .anyRequest().authenticated())
+                // As it is going to be a stateless API because of the use of JWT authentication
+                .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS));
 
         return http.build();
 
@@ -32,13 +35,33 @@ public class SecurityConfig {
 
     @Bean
     PasswordEncoder passwordEncoder() {
-        return new BCryptPasswordEncoder(); // Can be used anywhere else by injecting it into the class
+        return new BCryptPasswordEncoder(); // Can be used anywhere else by injecting it into that class
     }
 
     @Bean
     CorsConfigurationSource corsConfigurationSource() {
 
+        CorsConfiguration cors = new CorsConfiguration();
 
+        // Allowed Domains
+        cors.setAllowedOriginPatterns(List.of("${FRONTEND_URL}"));
+
+        // Allowed HTTP methods
+        cors.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
+
+        // Allowed HTTP headers
+        cors.setAllowedHeaders(List.of("Authorization", "Content-Type", "Accept"));
+        
+        // Allows cookies/auth token in the CORS
+        cors.setAllowCredentials(true);
+        
+        // This is used to map the CORS rule to a specific URL path in the backend
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+
+        // Register the CORS config to all backend endpoints("/**")
+        source.registerCorsConfiguration("/**", cors);
+        
+        return source;
 
     }
 
