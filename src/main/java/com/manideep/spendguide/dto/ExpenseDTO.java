b4418ebca98e60144e2709c808db558abb34588c
Manideep <deepanddipyo@gmail.com>
2025-08-12 23:34:16 +0530

    expense and income entity and DTO classes are added

---
+++ src/main/java/com/manideep/spendguide/dto/ExpenseDTO.java
@@ -0,0 +1,98 @@
+package com.manideep.spendguide.dto;
+
+import java.math.BigDecimal;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+
+public class ExpenseDTO {
+
+    private Long id;
+    private String name;
+    private String iconUrl;
+    private LocalDate date;
+    private BigDecimal amount;
+    private Long categoryId;
+    private String categoryName;
+    private LocalDateTime creationTime;
+    private LocalDateTime updationTime;
+    
+    public ExpenseDTO() {
+    }
+
+    public ExpenseDTO(Long id, String name, String iconUrl, LocalDate date, BigDecimal amount, Long categoryId,
+            String categoryName, LocalDateTime creationTime, LocalDateTime updationTime) {
+        this.id = id;
+        this.name = name;
+        this.iconUrl = iconUrl;
+        this.date = date;
+        this.amount = amount;
+        this.categoryId = categoryId;
+        this.categoryName = categoryName;
+        this.creationTime = creationTime;
+        this.updationTime = updationTime;
+    }
+
+    public Long getId() {
+        return id;
+    }
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getIconUrl() {
+        return iconUrl;
+    }
+    public void setIconUrl(String iconUrl) {
+        this.iconUrl = iconUrl;
+    }
+
+    public LocalDate getDate() {
+        return date;
+    }
+    public void setDate(LocalDate date) {
+        this.date = date;
+    }
+
+    public BigDecimal getAmount() {
+        return amount;
+    }
+    public void setAmount(BigDecimal amount) {
+        this.amount = amount;
+    }
+
+    public Long getCategoryId() {
+        return categoryId;
+    }
+    public void setCategoryId(Long categoryId) {
+        this.categoryId = categoryId;
+    }
+
+    public String getCategoryName() {
+        return categoryName;
+    }
+    public void setCategoryName(String categoryName) {
+        this.categoryName = categoryName;
+    }
+
+    public LocalDateTime getCreationTime() {
+        return creationTime;
+    }
+    public void setCreationTime(LocalDateTime creationTime) {
+        this.creationTime = creationTime;
+    }
+
+    public LocalDateTime getUpdationTime() {
+        return updationTime;
+    }
+    public void setUpdationTime(LocalDateTime updationTime) {
+        this.updationTime = updationTime;
+    }
+    
+}
---
+++ src/main/java/com/manideep/spendguide/entity/IncomeEntity.java
@@ -0,0 +1,141 @@
+package com.manideep.spendguide.entity;
+
+import java.math.BigDecimal;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.PrePersist;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "table_income")
+public class IncomeEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String name;
+
+    @Column(name = "icon_url")
+    private String iconUrl;
+
+    private LocalDate date;
+
+    private BigDecimal amount;
+
+    @Column(name = "creation_time", updatable = false)
+    @CreationTimestamp
+    private LocalDateTime creationTime;
+
+    @Column(name = "updation_time")
+    @UpdateTimestamp
+    private LocalDateTime updationTime;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(table = "category_id", nullable = false)
+    private CategoryEntity categoryEntity;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(table = "profile_id", nullable = false)
+    private ProfileEntity profileEntity;
+
+    public IncomeEntity() {
+    }
+    public IncomeEntity(Long id, String name, String iconUrl, LocalDate date, BigDecimal amount,
+            LocalDateTime creationTime, LocalDateTime updationTime, CategoryEntity categoryEntity,
+            ProfileEntity profileEntity) {
+        this.id = id;
+        this.name = name;
+        this.iconUrl = iconUrl;
+        this.date = date;
+        this.amount = amount;
+        this.creationTime = creationTime;
+        this.updationTime = updationTime;
+        this.categoryEntity = categoryEntity;
+        this.profileEntity = profileEntity;
+    }
+
+    public Long getId() {
+        return id;
+    }
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getIconUrl() {
+        return iconUrl;
+    }
+    public void setIconUrl(String iconUrl) {
+        this.iconUrl = iconUrl;
+    }
+
+    public LocalDate getDate() {
+        return date;
+    }
+    public void setDate(LocalDate date) {
+        this.date = date;
+    }
+
+    public BigDecimal getAmount() {
+        return amount;
+    }
+    public void setAmount(BigDecimal amount) {
+        this.amount = amount;
+    }
+
+    public LocalDateTime getCreationTime() {
+        return creationTime;
+    }
+    public void setCreationTime(LocalDateTime creationTime) {
+        this.creationTime = creationTime;
+    }
+
+    public LocalDateTime getUpdationTime() {
+        return updationTime;
+    }
+    public void setUpdationTime(LocalDateTime updationTime) {
+        this.updationTime = updationTime;
+    }
+
+    public CategoryEntity getCategoryEntity() {
+        return categoryEntity;
+    }
+    public void setCategoryEntity(CategoryEntity categoryEntity) {
+        this.categoryEntity = categoryEntity;
+    }
+
+    public ProfileEntity getProfileEntity() {
+        return profileEntity;
+    }
+    public void setProfileEntity(ProfileEntity profileEntity) {
+        this.profileEntity = profileEntity;
+    }
+
+    @PrePersist
+    public void setDatePrePersist() {
+        if (date == null) {
+            date = LocalDate.now();
+        }
+    }
+
+}
