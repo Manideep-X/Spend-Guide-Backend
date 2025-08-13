4abc1b5514267d52fa0c702bfab9e5d007a8ea8c
Manideep <deepanddipyo@gmail.com>
2025-08-14 00:00:52 +0530

    Service layer is modified and more methods are added

--- src/main/java/com/manideep/spendguide/service/ExpenseService.java
+++ src/main/java/com/manideep/spendguide/service/ExpenseService.java
@@ -1,7 +1,25 @@
 package com.manideep.spendguide.service;
 
+import java.math.BigDecimal;
+import java.util.List;
+
+import com.manideep.spendguide.dto.ExpenseDTO;
+
 public interface ExpenseService {
 
-    
+    // save a new expense to the DB for the current user.
+    public ExpenseDTO saveExpense(ExpenseDTO expenseDTO);
+
+    // Returns all expenses of current month for current user.
+    public List<ExpenseDTO> getForCurrMonthForCurrAcc();
+
+    // Delete an expense by it's ID.
+    public void deleteExpenseById(Long expenseId);
+
+    // Returns latest 5 expenses of current user.
+    public List<ExpenseDTO> getLatest5ForCurrAcc();
+
+    // Returns the sum of all expense of the current user.
+    public BigDecimal getTotalForCurrAcc();
 
 }
--- src/main/java/com/manideep/spendguide/service/IncomeService.java
+++ src/main/java/com/manideep/spendguide/service/IncomeService.java
@@ -1,7 +1,25 @@
 package com.manideep.spendguide.service;
 
+import java.math.BigDecimal;
+import java.util.List;
+
+import com.manideep.spendguide.dto.IncomeDTO;
+
 public interface IncomeService {
 
-    
+    // save a new Income to the DB for the current user
+    public IncomeDTO saveIncome(IncomeDTO incomeDTO);
+
+    // Returns all incomes of current month for current user.
+    public List<IncomeDTO> getForCurrMonthForCurrAcc();
+
+    // Delete an income by it's ID.
+    public void deleteIncomeById(Long incomeId);
+
+    // Returns latest 5 incomes of current user.
+    public List<IncomeDTO> getLatest5ForCurrAcc();
+
+    // Returns the sum of all incomes of current user.
+    public BigDecimal getTotalForCurrAcc();
 
 }
